c84cb7de1d4ea1b69e82dc5d4f649ae165c560b5
Ryan Lai <Ryan.Lai@armchina.com>
2022-10-14 17:34:54 +0800

    move internal path to test script (no opensource)
    Change-Id: I785e9849ffafc42b54d370de33ecf68f4f8cfe2f

--- test/lint_python.sh
+++ test/lint_python.sh
@@ -4,6 +4,7 @@ source /arm/tools/setup/init/bash
 module load swdev git/git/2.17.1
 module load swdev gnu/cmake/3.14.3
 module load swdev gnu/gcc/7.3.0
+module load swdev python/python/3.8.5
 
 cd ../
 
