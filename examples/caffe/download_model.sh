db072fe812bdb57f33778e489d7e4527cea77366
Runner Zhong <runner.zhong@armchina.com>
2022-10-20 14:49:43 +0800

    [DOC]Add onnx & caffe example
    Change-Id: I04ce6f02edc2cd84ec7da561fa8a8b8ff92117a3

---
+++ examples/caffe/download_model.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget -O alexnet.caffemodel http://dl.caffe.berkeleyvision.org/bvlc_alexnet.caffemodel
+
+wget -O alexnet.prototxt https://github.com/BVLC/caffe/blob/master/models/bvlc_alexnet/deploy.prototxt
---
+++ examples/onnx/download_model.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+wget -O mobilenet_v2.onnx https://github.com/onnx/models/blob/main/vision/classification/mobilenet/model/mobilenetv2-10.onnx
