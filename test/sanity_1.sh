9bd289de57225402b5becfaeba5cbd627080fa9c
Neo Wang <neo.wang@armchina.com>
2022-10-25 11:07:50 +0800

    - add sanity back
    Change-Id: I71575f5315df9a11932875ded780b674e64cf73e

---
+++ test/sanity_1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+wk_dir=`pwd`
+echo ${wk_dir}
+ls
+source /arm/tools/setup/init/bash
+module load swdev python/python/3.8.5
+
+export LD_LIBRARY_PATH=/arm/tools/gnu/gcc/7.3.0/rhe7-x86_64/lib64:$LD_LIBRARY_PATH
+cd sanity
+python3 ./sanity_test.py
