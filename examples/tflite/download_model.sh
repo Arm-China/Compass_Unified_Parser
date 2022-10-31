efe98d456f17047ab3774658d891155692b64832
Runner Zhong <runner.zhong@armchina.com>
2022-10-31 17:36:37 +0800

    Fix run_example import issue and download path issue
    Change-Id: I48e46ee0e337bbaf52cd79d7262e34e513ace3a9

--- examples/tflite/download_model.sh
+++ examples/tflite/download_model.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+cd `dirname $0`; pwd
 wget https://storage.googleapis.com/download.tensorflow.org/models/tflite_11_05_08/mobilenet_v2_1.0_224.tgz
 
 tar -zxvf ./mobilenet_v2_1.0_224.tgz
