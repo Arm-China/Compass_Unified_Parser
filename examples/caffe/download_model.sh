e525735a794c79ec6ca010ff621198f87182ef6d
Runner Zhong <runner.zhong@armchina.com>
2022-11-01 13:19:36 +0800

    [Doc]Use raw links in example test for github HTML wrapper
    Change-Id: I98349b72acf90580551c31fb1b04d311acf3afb5

--- examples/caffe/download_model.sh
+++ examples/caffe/download_model.sh
@@ -3,4 +3,4 @@
 cd `dirname $0`; pwd
 wget -O alexnet.caffemodel http://dl.caffe.berkeleyvision.org/bvlc_alexnet.caffemodel
 
-wget -O alexnet.prototxt https://github.com/BVLC/caffe/blob/master/models/bvlc_alexnet/deploy.prototxt
+wget -O alexnet.prototxt https://raw.githubusercontent.com/BVLC/caffe/master/models/bvlc_alexnet/deploy.prototxt
