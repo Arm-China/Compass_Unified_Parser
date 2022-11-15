208dd2aa1cce73694982f78bf26f598a332779f1
Yuxian Chen <yuxian.chen@armchina.com>
2022-11-15 17:39:59 +0800

    Do not raise warning for multiple axes in tf ReverseV2 op if inputs are constant
    Change-Id: I9e5cae5602bfc54e769360c951007a844c492bb4

--- test/op_test/array_ops/test_reverse-tf.py
+++ test/op_test/array_ops/test_reverse-tf.py
@@ -8,12 +8,16 @@ import tensorflow.compat.v1 as tf
 from utils.run import run_parser
 
 
-def create_reverse_model(pb_file_path, input_size, axis):
+def create_reverse_model(pb_file_path, input_size, axis, is_const_input=False):
     ''' Create tensorflow model for reverse op.
     '''
     with tf.Session(graph=tf.Graph()) as sess:
         x = tf.placeholder(tf.float32, shape=input_size, name='X')
-        op1 = tf.raw_ops.ReverseV2(tensor=x, axis=axis, name='reverse')
+        if is_const_input:
+            inp = tf.constant(np.random.ranf(input_size), tf.float32)
+        else:
+            inp = x
+        op1 = tf.raw_ops.ReverseV2(tensor=inp, axis=axis, name='reverse')
         y = tf.math.add(op1, x, name='Y')
 
         sess.run(tf.global_variables_initializer())
@@ -34,12 +38,12 @@ feed_dict = dict()
 for input_shape, axis in zip(input_shapes, axes):
     feed_dict.clear()
     feed_dict['X:0'] = np.random.ranf(input_shape).astype(np.float32)
+    for is_const_input in (True, False):
+        model_path = '-'.join([TEST_NAME, str(len(input_shape)), str(is_const_input)]) + '.pb'
+        # Create model
+        create_reverse_model(model_path, input_shape, axis, is_const_input)
 
-    model_path = '-'.join([TEST_NAME, str(len(input_shape))]) + '.pb'
-    # Create model
-    create_reverse_model(model_path, input_shape, axis)
-
-    # Run tests with parser and compare result with runtime
-    exit_status = run_parser(
-        model_path, feed_dict, model_type='tf', verify=True)
-    assert exit_status
+        # Run tests with parser and compare result with runtime
+        exit_status = run_parser(
+            model_path, feed_dict, model_type='tf', verify=True)
+        assert exit_status
