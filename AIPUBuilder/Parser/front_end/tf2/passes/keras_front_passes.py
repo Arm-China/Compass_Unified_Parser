CB-4312: Fix similarity issue of keras Normalization op with variance=0
Refer to keras/layers/preprocessing/normalization.py, the output should be
(inputs - self.mean) / tf.maximum(tf.sqrt(self.variance), backend.epsilon()),
in which backend.epsilon() is 1e-07.

Change-Id: I3a8fc720f3c0b2a0d1a0b3fb73b2dc57dc70b984

AIPUBuilder/Parser/front_end/tf2/passes/keras_front_passes.py
@@ -465,7 +465,7 @@ def convert_normalization(graph):
             if in_attr['tensor'] is not None and in_attr['tensor'].value is not None:
                 sub_out_attr['tensor'].value = in_attr['tensor'].value - mean
             graph.add_edge(sub, norm, **sub_out_attr)
-            mul_value = np.array(1 / np.sqrt(variance))
+            mul_value = np.array(1 / np.maximum(np.sqrt(variance), 1e-7))
             insert_constant(graph, norm + '_mul', mul_value, norm, in_port=1)
 
             NodeWrap(graph, sub).replace_obj('Sub', {'name': sub, 'opset_version': 13})