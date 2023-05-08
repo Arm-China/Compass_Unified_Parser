c1f438e3f4cdfe68bb53c709ee3533e85f6f60a6
Yuxian Chen <yuxian.chen@armchina.com>
2023-05-08 14:24:07 +0800

    CB-4719: Add int8 to cast_in_ports of ArmDepthwiseConvOp
    Change-Id: Ief4634b43ff4d3b448f5a1e1aac056a9bfb01a86

--- AIPUBuilder/Parser/ops/release_ops.py
+++ AIPUBuilder/Parser/ops/release_ops.py
@@ -1470,7 +1470,7 @@ class ArmDepthToSpaceOp(OpHasOneOutPort, ArmOp):
 class ArmDepthwiseConvOp(BaseActivationOp, BaseConvOp, ArmOp):
     @classmethod
     def cast_in_ports(cls):
-        return {0: 'float32'}
+        return {0: ['float32', 'int8']}
 
     @classmethod
     def attributes(cls):
