bdf7fb9515306d48e58c0bbab4c537abab6cd4a8
Yuxian Chen <yuxian.chen@armchina.com>
2023-06-14 15:39:17 +0800

    [leaky_relu] Remove edges for tf2 leaky_relu and relu op
    Change-Id: I023680b7aaf8f396b7e0148ae96531a1b97acfe9

--- AIPUBuilder/Parser/front_end/tf2/passes/front_passes.py
+++ AIPUBuilder/Parser/front_end/tf2/passes/front_passes.py
@@ -395,9 +395,10 @@ def convert_to_onnx(graph):
                 size = node_obj.depth_radius * 2 + 1
                 alpha = node_obj.alpha * size
                 new_node_attr.update({'size': size, 'alpha': alpha})
-            elif pure_type in ('log_softmax', 'overlap_and_add', 'reduce_all', 'reduce_any', 'reduce_logsumexp', 'reduce_max',
+            elif pure_type in ('leaky_relu', 'log_softmax', 'overlap_and_add',
+                               'reduce_all', 'reduce_any', 'reduce_logsumexp', 'reduce_max',
                                'reduce_mean', 'reduce_min', 'reduce_prod', 'reduce_sum', 'reduce_variance',
-                               'split'):
+                               'relu', 'split'):
                 _remove_edges_if_const(node_name, in_edges[1:])
             elif pure_type == 'right_shift':
                 new_node_attr.update({'direction': 'RIGHT'})
