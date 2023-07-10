41e10a75ec0ea16feeed7c6cd22fa494360c5d3c
Yuxian Chen <yuxian.chen@armchina.com>
2023-07-10 16:36:54 +0800

    CB-5035: Support torch add/sub/rsub with alpha!=1
    Change-Id: Ifd0fa16473d4f1e2b1f98c7e4e91b1d6cb436e49

--- test/op_test/math_ops/test_add-torch.py
+++ test/op_test/math_ops/test_add-torch.py
@@ -4,13 +4,18 @@ from utils.run import run_parser
 
 
 class add_model(torch.nn.Module):
+    def __init__(self, add_alpha=1):
+        super(add_model, self).__init__()
+        self.add_alpha = add_alpha
+
     def forward(self, x, y):
-        return torch.add(x, y, alpha=1)
+        x_sub = torch.sub(y, x, alpha=3)
+        return torch.add(x_sub, y, alpha=self.add_alpha)
 
 
-def create_add_model(model_path):
+def create_add_model(model_path, alpha):
     try:
-        model = add_model()
+        model = add_model(alpha)
         model_scripted = torch.jit.script(model)
         model_scripted.save(model_path)
     except Exception as e:
@@ -18,12 +23,13 @@ def create_add_model(model_path):
 
 
 TEST_NAME = 'add'
-model_path = TEST_NAME + '.pt'
 
-# prepare model and input datas
-create_add_model(model_path)
 x_data = np.random.randint(-10, 20, [2, 16, 5]).astype(np.int32)
 y_data = np.random.randint(-20, 30, [1, 16, 5]).astype(np.int32)
 feed_dict = {'x': x_data, 'y': y_data}
-exit_status = run_parser(model_path, feed_dict, expected_keywords=['layer_top_type=[int32]'])
-assert exit_status
+
+for alpha in (1, 5, -4):
+    model_path = '-'.join([TEST_NAME, str(alpha)]) + '.pt'
+    create_add_model(model_path, alpha)
+    exit_status = run_parser(model_path, feed_dict, expected_keywords=['layer_top_type=[int32]'])
+    assert exit_status
