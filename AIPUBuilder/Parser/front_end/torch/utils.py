e1e317191793f1353239ef0c80fc0df68a64747f
siyzho01 <Siyan.Zhou@armchina.com>
2023-07-17 20:52:55 +0800

    [Parser] torch qat shufflenetv2 part1
    Change-Id: Ib10cefb9cc8ecec7442446688280790001018e98

--- AIPUBuilder/Parser/front_end/torch/utils.py
+++ AIPUBuilder/Parser/front_end/torch/utils.py
@@ -3,6 +3,108 @@
 
 
 import torch
+import torch.onnx.symbolic_helper as helper
+import functools
+
+
+def quantize_helper(
+    g,
+    tensor,
+    scale,
+    zero_point,
+    axis=None,
+):
+    if (
+        axis is not None
+        and not _is_none(axis)
+        and GLOBALS.export_onnx_opset_version < 13
+    ):
+        _onnx_opset_unsupported_detailed(
+            'QuantizeLinear',
+            GLOBALS.export_onnx_opset_version,
+            13,
+            'Attribute axis is not supported.',
+        )
+
+    assert tensor is not None
+    if tensor.type().scalarType() is None:
+        tensor = g.op('Cast', tensor,
+                      to_i=torch._C._onnx.TensorProtoDataType.FLOAT)
+
+    assert scale is not None
+    if scale.type().scalarType() != 'Float':
+        scale = g.op(
+            'Cast', scale, to_i=torch._C._onnx.TensorProtoDataType.FLOAT)
+
+    assert zero_point is not None
+    if zero_point.type().scalarType() not in ('Byte', 'Char'):
+        zero_point = g.op('Cast', zero_point,
+                          to_i=torch._C._onnx.TensorProtoDataType.UINT8)
+    output = g.op(
+        'QuantizeLinear',
+        tensor,
+        scale,
+        zero_point,
+        axis_i=helper._get_const(axis, 'i', 'axis'),
+    )
+    args = [output, scale, zero_point]
+    if axis is not None and not _is_none(axis):
+        args.append(axis)
+    return g.op('prim::TupleConstruct', *args)
+
+
+def quantize_helper_multi(
+    g,
+    tensors,
+    scale,
+    zero_point,
+    axis=None,
+):
+    if (
+        axis is not None
+        and not _is_none(axis)
+        and GLOBALS.export_onnx_opset_version < 13
+    ):
+        _onnx_opset_unsupported_detailed(
+            'QuantizeLinear',
+            GLOBALS.export_onnx_opset_version,
+            13,
+            'Attribute axis is not supported.',
+        )
+
+    assert scale is not None
+    if scale.type().scalarType() != 'Float':
+        scale = g.op(
+            'Cast', scale, to_i=torch._C._onnx.TensorProtoDataType.FLOAT)
+
+    assert zero_point is not None
+    if zero_point.type().scalarType() not in ('Byte', 'Char'):
+        zero_point = g.op('Cast', zero_point,
+                          to_i=torch._C._onnx.TensorProtoDataType.UINT8)
+
+    res = []
+    for tensor in tensors:
+        assert tensor is not None
+        if tensor.type().scalarType() is None:
+            tensor = g.op('Cast', tensor,
+                          to_i=torch._C._onnx.TensorProtoDataType.FLOAT)
+
+        args = []
+        output = g.op(
+            'QuantizeLinear',
+            tensor,
+            scale,
+            zero_point,
+            axis_i=helper._get_const(axis, 'i', 'axis'),
+        )
+        args.append(output)
+        args.append(scale)
+        args.append(zero_point)
+        if axis is not None and not _is_none(axis):
+            args.append(axis)
+        res.append(g.op('prim::TupleConstruct', *args))
+
+    return res
 
 
 def get_tuple_from_tensor_type(torch_type, tensor_list, start_index=0):
