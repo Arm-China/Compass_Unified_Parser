689924e6d8f8140c3ff50a894cf58116c4147bde
Yuxian Chen <yuxian.chen@armchina.com>
2023-11-01 10:33:07 +0800

    [cropping] CB-5695: Fix __getattr__ of SliceOp
    Use get_input_shapes() to get default value of axes and steps for Slice op.
    
    Change-Id: I9c753346b9495b9b2664d4707cbe4a74ae2a8ca4

---
+++ test/op_test/array_ops/test_slice-tf.py
@@ -0,0 +1,42 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright © 2022-2023 Arm Technology (China) Co. Ltd.
+
+import numpy as np
+
+import tensorflow.compat.v1 as tf
+
+from utils.run import run_parser
+
+
+def create_slice_model(pb_file_path, input_size):
+    ''' Create tensorflow model for slice op.
+    '''
+    with tf.Session(graph=tf.Graph()) as sess:
+        x = tf.placeholder(tf.float32, shape=input_size, name='X')
+        op1 = tf.slice(x, [1, 0, 0, 2], [5, 6, 7, 8], name='slice')
+        y = tf.add(op1, 10.0, name='Y')
+
+        sess.run(tf.global_variables_initializer())
+        constant_graph = tf.graph_util.convert_variables_to_constants(
+            sess, sess.graph_def, ['Y'])
+
+        # save to pb file
+        with tf.gfile.GFile(pb_file_path, mode='wb') as f:
+            f.write(constant_graph.SerializeToString())
+
+
+TEST_NAME = 'slice'
+input_shape = [10, 12, 20, 30]
+
+# Generate input data
+feed_dict = dict()
+feed_dict['X:0'] = np.random.ranf(input_shape).astype(np.float32)
+
+model_path = TEST_NAME + '.pb'
+# Create model
+create_slice_model(model_path, input_shape)
+
+# Run tests with parser and compare result with runtime
+exit_status = run_parser(
+    model_path, feed_dict, model_type='tf', verify=True)
+assert exit_status
