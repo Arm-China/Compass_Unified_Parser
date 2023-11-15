53a7f83529dff3b382213b31ef64011213d008b1
Yuxian Chen <yuxian.chen@armchina.com>
2023-11-15 10:46:26 +0800

    [gru] CB-5798: Fix similarity issue of hidden output in GRU op
    For onnx bidirectional GRU, the hidden output is to concat fw and bw,
    and bw is not reversed.
    
    Change-Id: Ifd8e1122be69bc5309aba51003dc37ab0857cc9e

--- test/op_test/nn_ops/test_gru-torch.py
+++ test/op_test/nn_ops/test_gru-torch.py
@@ -7,7 +7,7 @@ from utils.run import run_parser
 class gru_model(nn.Module):
     def __init__(self):
         super(gru_model, self).__init__()
-        self.rnn = nn.GRU(10, 20, 2)
+        self.rnn = nn.GRU(10, 20, 2, bias=True, bidirectional=True)
 
     def forward(self, x1, x2):
         return self.rnn(x1, x2)
@@ -28,7 +28,7 @@ model_path = TEST_NAME + '.pt'
 # prepare model and input datas
 create_torch_model(model_path)
 x1_data = np.random.ranf([5, 3, 10]).astype(np.float32)
-x2_data = np.random.ranf([2, 3, 20]).astype(np.float32)
+x2_data = np.random.ranf([4, 3, 20]).astype(np.float32)
 feed_dict = {'x1': x1_data, 'x2': x2_data}
 exit_status = run_parser(model_path, feed_dict)
 assert exit_status
