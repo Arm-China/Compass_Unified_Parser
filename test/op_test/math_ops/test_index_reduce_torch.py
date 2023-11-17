c1c62c88707b4393e1a7ae90c4c3e09f48b9682e
Yuxian Chen <yuxian.chen@armchina.com>
2023-11-17 14:58:53 +0800

    CB-5728, CB-5727: Fix convert_index_reduce and support all the reduction
    Change-Id: I7595c152a0249990ce6de48a03c0480fdabba799

--- test/op_test/math_ops/test_index_reduce_torch.py
+++ test/op_test/math_ops/test_index_reduce_torch.py
@@ -4,19 +4,18 @@ from utils.run import run_parser
 
 
 class reduce_model(torch.nn.Module):
-    def __init__(self, reduce='prod', include_self=False, index=torch.tensor([0, 1]), t=torch.arange(start=0, end=192, step=1,
-                                                                                                     dtype=torch.float).resize(4, 6, 8)):
+    def __init__(self, reduce, include_self):
         super(reduce_model, self).__init__()
         self.reduce = reduce
         self.include_self = include_self
 
-    def forward(self, x, index, t):
+    def forward(self, x, t, index):
         return x.index_reduce_(-3, index, t, self.reduce, include_self=self.include_self)
 
 
-def create_index_reduce_model(model_path, reduce, include_self, index, t):
+def create_index_reduce_model(model_path, reduce, include_self):
     try:
-        model = reduce_model(reduce, include_self, index, t)
+        model = reduce_model(reduce, include_self)
         model_scripted = torch.jit.script(model)
         model_scripted.save(model_path)
     except Exception as e:
@@ -26,22 +25,20 @@ def create_index_reduce_model(model_path, reduce, include_self, index, t):
 TEST_NAME = 'index_reduce'
 
 x_data = np.random.ranf([10, 6, 8]).astype(np.float32)
-# x_data = np.random.randint(1, 5, [10, 6, 8]).astype(np.float32)
 
 index_data = (np.array([0, 0, 1, 2]), np.array([0, 2, 1, 3]))
 t = torch.arange(start=0, end=192, step=1,
                  dtype=torch.float).resize(4, 6, 8).numpy()
+feed_dict = {'x': x_data, 't': t}
 
 
 for index in index_data:
-    feed_dict = {'x': x_data, 'index': index, 't': t}
+    feed_dict.update({'index': index})
     for include_self in (False, True):
-        for reduce in ('amin', 'amin', 'prod', 'mean',):  # 'amax''amin'
-            if reduce == 'mean':
-                continue
+        for reduce in ('prod', 'mean', 'amin', 'amin', ):
             model_path = '-'.join([TEST_NAME, str(reduce),
                                    str(include_self)]) + '.pt'
             create_index_reduce_model(
-                model_path, reduce, include_self, index, t)
+                model_path, reduce, include_self)
             exit_status = run_parser(model_path, feed_dict)
             assert exit_status
