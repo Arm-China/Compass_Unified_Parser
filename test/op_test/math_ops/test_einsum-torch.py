83f778ffd78278bbc14e0a9b353e801739c00057
Runner Zhong <runner.zhong@armchina.com>
2024-01-31 17:44:52 +0800

    [Parser]fix AIAE-244 again
    Change-Id: Id3f46800e587d1b77f411fbb70aa23bd9b96c707

--- test/op_test/math_ops/test_einsum-torch.py
+++ test/op_test/math_ops/test_einsum-torch.py
@@ -24,16 +24,23 @@ def create_einsum_model(model_path, equation):
 
 TEST_NAME = 'einsum'
 model_path = TEST_NAME + '.pt'
-shape_dict = {'i': 10, 'j': 12, 'k': 14, 'l': 15, 'm': 9, 'n': 8}
+shape_dict = {'i': 10, 'j': 12, 'k': 14, 'l': 15, 'm': 9, 'n': 8,
+              'a': 4, 'b': 5}
 
-for equation in ('nijkl,imkln->ikjnm', 'ijknl,niljm->nikjm',
-                 'ijkl,imkl->ikjm', 'ijkl,iljm->ikjm',
-                 'mij,jk->mik', 'ij,mkj->mik', 'mij,mjk->mik', 'mij,mkj->mik',
-                 'l i j, l j k -> l i k',
-                 'l m n i k, l m n j k -> l m n i j',
-                 'l m n i j, l m n j k -> l m n i k',
-                 'ij,jk->ik', 'ij,kj->ik', ):
-    input1, input2 = equation.split('->')[0].split(',')
+for equation in (
+        'nijkl,imkln->ikjnm', 'ijknl,niljm->nikjm',
+        'ijkl,imkl->ikjm', 'ijkl,iljm->ikjm',
+        'mij,jk->mik', 'ij,mkj->mik', 'mij,mjk->mik', 'mij,mkj->mik',
+        'l i j, l j k -> l i k',
+        'l m n i k, l m n j k -> l m n i j',
+        'l m n i j, l m n j k -> l m n i k',
+        'ij,jk->ik', 'ij,kj->ik',
+        'ijkl,ilm->ijkm', '...i,ji->...ji', 'ijkl,ijkm->ilm',):
+    if '...' in equation:
+        new_equation = equation.replace('...', 'ab')
+    else:
+        new_equation = equation
+    input1, input2 = new_equation.split('->')[0].split(',')
     input1_shape = [shape_dict[s] for s in input1 if s in shape_dict]
     input2_shape = [shape_dict[s] for s in input2 if s in shape_dict]
     x1_data = np.random.ranf(input1_shape)
