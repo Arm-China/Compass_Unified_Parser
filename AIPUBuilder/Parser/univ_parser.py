7c165bdf6094dd93f92095b53f6065ac14b27087
Guanming Xu <guanming.xu@armchina.com>
2024-02-02 10:48:53 +0800

    [Parser] rearrange check_similarity
    Change-Id: I712bb1d1a882b3729d86b896efb49e9a3b250e2e

--- AIPUBuilder/Parser/univ_parser.py
+++ AIPUBuilder/Parser/univ_parser.py
@@ -11,92 +11,6 @@ from .common.utils import is_file, is_dir, multi_string_to_list, list_string_to_
 from .logger import *
 
 
-def check_similarity(graph, params, txt_path, bin_path):
-    if not params.get('similarity_input_npy', ''):
-        return True
-
-    if not txt_path or not bin_path or not is_file(txt_path) or not is_file(bin_path):
-        ERROR('[Parser]: Meets invalid txt_path (%s) or bin_path (%s) in check_similarity!' % (txt_path, bin_path))
-        return False
-
-    model_path = params.get('original_input_model', params.get('input_model', ''))
-    if not model_path:
-        ERROR('[Parser]: Meets empty input_model in check_similarity!')
-        return False
-
-    feed_dict = params['similarity_input_npy']
-    DEBUG('[Parser]: Input names from feed_dict: %s' % str(list(feed_dict.keys())))
-    forward_type = 'float'
-
-    if graph._attr.get('quantize', False):
-        from .utils.quantize import generate_symm_quant_cfg, generate_symm_quant_ir
-        model_name = params.get('model_name', 'quant')
-        symm_quant_cfg_file = generate_symm_quant_cfg(model_name, txt_path, bin_path)
-        if not symm_quant_cfg_file:
-            ERROR('[Parser]: Fail to generate symm quant cfg file!')
-            return False
-        if not generate_symm_quant_ir(symm_quant_cfg_file):
-            ERROR('[Parser]: Fail to generate symm quant IR!')
-            return False
-        new_base_path = os.path.join(os.path.dirname(txt_path), model_name + '_opt')
-        txt_path = new_base_path + '.txt'
-        if not is_file(txt_path):
-            ERROR('[Parser]: Meets invalid symm quant txt(%s) file!' % txt_path)
-            return False
-        INFO('[Parser]: symm quant txt file: %s' % txt_path)
-        bin_path = new_base_path + '.bin'
-        if not is_file(bin_path):
-            ERROR('[Parser]: Meets invalid symm quant bin(%s) file!' % bin_path)
-            return False
-        INFO('[Parser]: symm quant bin file: %s' % bin_path)
-        forward_type = 'quantized'
-
-    ret = True
-    rt_output_dict, opt_output_dict = {}, {}
-    try:
-        from .utils.forward import rt_forward
-        # Get model output using runtime of original framework(tf, onnx, caffe and etc)
-        output_names = params.get('output_names', None)
-        rt_output_dict = rt_forward(model_path, feed_dict, output_names=output_names if output_names else None,
-                                    proto_path=params.get('caffe_prototxt', ''))
-    except Exception as e:
-        ERROR('[Parser]: Meets Exception (%s) in framework runtime forward!' % str(e))
-        ret = False
-
-    transfer_to_float = True
-    if forward_type == 'quantized':
-        for value in rt_output_dict.values():
-            if 'int' in value.dtype.name:
-                transfer_to_float = False
-                break
-
-    try:
-        from .utils.forward import opt_forward
-        # Get model output using opt forward
-        opt_output_dict = opt_forward(txt_path, bin_path, feed_dict, forward_type=forward_type,
-                                      transfer_to_float=transfer_to_float)
-    except Exception as e:
-        ERROR('[Parser]: Meets Exception (%s) in opt forward!' % str(e))
-        ret = False
-
-    if not rt_output_dict or not opt_output_dict:
-        ERROR('[Parser]: Fail to check similarity due to missing runtime/opt forward outputs!')
-        ret = False
-    else:
-        from .utils.compare import compare_data_dict
-        # Compare outputs
-        INFO('[Parser]: Comparing outputs(first runtime, second opt)')
-        ret = compare_data_dict(rt_output_dict, opt_output_dict)
-
-        # Report result
-        if ret:
-            INFO('[Parser]: Similarity checking is passed!')
-        else:
-            WARN('[Parser]: Similarity checking is failed!')
-            ret = False
-    return ret
-
-
 def univ_parser(params):
     ret = True
 
@@ -257,7 +171,7 @@ def univ_parser(params):
                 from .graph.pattern_match import single_node_matcher
                 from .writer import serialize, show_in_out_map
                 from .preprocess import gamut_preprocess, preprocess
-                from .misc import special_character_conversion
+                from .misc import special_character_conversion, check_similarity
 
                 '''Check if it is a connected graph.'''
                 input_names = []
