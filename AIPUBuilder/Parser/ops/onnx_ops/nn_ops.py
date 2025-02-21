3d22eace1a7ae9e0428df7e3f18abe3c7a7e3de3
Runner Zhong <runner.zhong@armchina.com>
2025-02-21 16:52:16 +0800

    [Parser]CP-15332, support DeepSeek 1.5B from onnx community
    Change-Id: I8558cae70c83c5b352fc08884a1ab91b8741fd41

--- AIPUBuilder/Parser/ops/onnx_ops/nn_ops.py
+++ AIPUBuilder/Parser/ops/onnx_ops/nn_ops.py
@@ -2127,3 +2127,62 @@ class ReluOp(BaseReluOp, OnnxOp):
         inputs = self.get_input_tensors()
         out_tensor = self.cal_activation(inputs[0])
         self.set_out_tensor(out_tensor)
+
+
+class SimplifiedLayerNormalizationOp(OpHasAxis, OpHasVariableOutPorts, OnnxOp):
+    @classmethod
+    def attributes(cls):
+        return {
+            1: {
+                'axis': {
+                    'type': AttrType.INT, 'required': True
+                },
+                'epsilon': {
+                    'type': AttrType.FLOAT, 'required': True
+                },
+                'stash_type': {
+                    'type': AttrType.INT, 'required': True
+                },
+            }
+        }
+
+    def __getattr__(self, item):
+        ret = None
+        if item == 'axes':
+            ret = self.__dict__['_attr'][item].value
+            if ret is None and self.axis is not None:
+                input_length = len(self.get_input_shapes()[0])
+                start_axis = (self.axis + input_length) if self.axis < 0 else self.axis
+                ret = [axis for axis in range(input_length) if axis >= start_axis]
+                self.__dict__['_attr'][item].value = ret
+        if ret is None:
+            ret = super(SimplifiedLayerNormalizationOp, self).__getattr__(item)
+        return ret
+
+    def __init__(self, graph, attr_dict=None):
+        super(SimplifiedLayerNormalizationOp, self).__init__(graph, attr_dict)
+        self.update_attributes(SimplifiedLayerNormalizationOp, attr_dict)
+        assert self.check_required(), 'SimplifiedLayerNormalizationOp is missing a required parameter.'
+
+    def infer_shape(self):
+        super(SimplifiedLayerNormalizationOp, self).infer_shape()
+        inputs = self.get_input_tensors()
+        assert len(inputs) == 2, f'SimplifiedLayerNormalizationOp should have 2 inputs, but got {len(inputs)}'
+
+        input = inputs[0].astype(np.float32) if self.stash_type == 1 else inputs[0]
+        scale = inputs[1]
+
+        # RMSNorm
+        square = np.power(input, 2)
+        std_var = np.mean(square, axis=tuple(self.axes), keepdims=True)
+        normalized = input / np.sqrt(std_var + self.epsilon)
+        if self.stash_type == 1:
+            normalized = normalized.astype(inputs[0].dtype)
+        axes = OpHasAxis.make_axes_non_negative(
+            self.axes, len(input.shape))
+        weights = OpHasAxis.expand_to(scale, axes, len(input.shape))
+        out_tensors = [(normalized * weights).astype(inputs[0].dtype)]
+        out_ports = self.get_out_ports()
+        if 1 in out_ports:
+            out_tensors.append(np.array(std_var, np.float32))
+        self.set_out_tensor(out_tensors)
